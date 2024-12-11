3488474f61de16bb6fa9b8d11d6aa1e3e8fe12c7
DimmuA7X <juanjo.pm.88@gmail.com>
2024-12-11 11:57:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const excuses = [
-    "Hoy no puedo, mi pez murió y estoy triste.",
-    "Hace frío, mejor mañana.",
-    "Mi smartwatch no tiene batería, ¿cómo voy a contar las calorías?",
-    "Estoy practicando descanso activo desde el sofá.",
-    "La barra me mira mal, paso."
+    "Hoy no puedo, Juanjo ha dejado caer la barra y se rompió mi motivación.",
+    "Diego está demasiado motivado, me intimida y prefiero descansar.",
+    "Adri dijo que hoy era día de descanso... y no voy a llevarle la contraria.",
+    "Ángel prometió venir, pero está en la esquina haciendo selfies.",
+    "El gimnasio está cerrado por mantenimiento... según mi imaginación.",
+    "Hoy no es mi día, el banco de press me mira mal.",
+    "Mi proteína caducó y no puedo entrenar sin ella, es mi combustible."
 ];
 
 document.getElementById("excuse-button").addEventListener("click", () => {
